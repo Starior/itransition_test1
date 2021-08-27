b82d4eb7a26380351649687c39643d2ab5a4a891
Starior <58112940+Starior@users.noreply.github.com>
2021-08-27 12:21:02 +0300

    Update lcs_RAW.js

--- lcs_RAW.js
+++ lcs_RAW.js
@@ -2,7 +2,7 @@ function c(s) {
   if (!s.length) return '';
   let h = s.reduce((a,b) => a.length <= b.length ? a :  b), m = h.length;
   for (let l = m; l > 0; l--){
-    for (let b = 0; b<= m-l; b++){
+    for (let b = 0; b <= m-l; b++){
       let o = h.substring(b, b+l);
       if (s.every( e=> e.includes(o)))
         return o;
