3e71b057f2eaf7b9be8396ab16dd7d5f0359df4c
Starior <58112940+Starior@users.noreply.github.com>
2021-08-27 12:17:35 +0300

    Add files via upload

---
+++ lcs.js
@@ -0,0 +1,1 @@
+function c(s){if(!s.length)return'';let h=s.reduce((a,b)=>a.length<=b.length?a:b),m=h.length;for(let l=m;l>0;l--){for(let b=0;b<=m-l;b++){let o=h.substring(b,b+l);if(s.every(e=>e.includes(o)))return o;}}return''}console.log(c(process.argv.slice(2)))
